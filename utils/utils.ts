4b0fbe721bc8b4b2c119b1dd86920bab282a8a93
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-02-05 09:42:44 +0100

    added general utils with regex

---
+++ utils/utils.ts
@@ -0,0 +1,14 @@
+export function checkMailFormat(mail: string): boolean{
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(mail);
+}
+
+export function checkPasswordFormat(password: string): boolean{
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return regex.test(password);
+}
+
+export function checkStringFormat(username: string): boolean{
+    const regex = /^[a-zA-Z0-9]{3,}$/;
+    return regex.test(username);
+}
