f6863d9e13289282478621801767f11d68c7c884
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-02-06 09:42:55 +0100

    added base of locking system

---
+++ public/src/tasklist.ts
@@ -0,0 +1,5 @@
+export function noAccessPopUp(): void {
+}
+
+export function showTasklist(listID: number): void {
+}
--- src/collaboration.ts
+++ src/collaboration.ts
@@ -1,6 +1,7 @@
 // send mail to specific user with nodemailer
 import * as nodemailer from 'nodemailer';
 import * as utils from './utils';
+import * as tasklist from '../public/src/tasklist';
 
 export function sendMailToUser(receiver: string, message: string, subj: string): void {
     console.log(`Mail sent to ${receiver} with message: ${message}`)
@@ -23,8 +24,20 @@ export function sendMailToUser(receiver: string, message: string, subj: string):
 export function sendInviteMail(receiver: string, listID: number): void {
     const hashedID: string = utils.hashString(listID.toString());
     const link: string = `http://localhost:3000/signup?listID=${hashedID}`;
-    const message: string = `Hello my dear friend! You have been invited to join a tasklist on our platform. Please click on the link to sign up.`;
+    const message: string = `Hello my dear friend!\nYou have been invited to join a tasklist on our platform.\n Please click on the link to sign up.` + link;
     sendMailToUser(receiver, message, 'Tasklist Invitation');
     // save link in local storage for use after signup
     localStorage.setItem('listID', listID.toString()); 
+}
+
+export function tryEditTasklist(receiver: string, listID: number): void {
+    // send get request to server to get the tasklist
+    const isLocked = false; 
+    // TODO: get from server with receiver and listID 
+    if (isLocked) {
+        tasklist.noAccessPopUp();
+    } else {
+        // TODO: set isLocked to true on server
+        tasklist.showTasklist(listID);
+    }
 }
