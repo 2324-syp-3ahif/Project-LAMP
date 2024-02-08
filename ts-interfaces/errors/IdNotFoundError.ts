a214c25d40f33554e541e27135cd4c4971411d91
HaasLuca <l.haas@students.htl-leonding.ac.at>
2024-02-08 16:51:54 +0100

    Added some Errors

---
+++ ts-interfaces/errors/IdNotFoundError.ts
@@ -0,0 +1,7 @@
+export class IdNotFoundError extends Error {
+    constructor(message?: string) {
+        super(message);
+        this.name = 'IdNotFoundError';
+        this.message = message ?? "";
+    }
+}
