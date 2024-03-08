db05914624209146b3eaa0f4ff6722a5d9489661
RaabPhilip <114643660+RaabPhilip@users.noreply.github.com>
2024-03-08 12:54:54 +0100

    fixed the statuscodes

--- routers/router-tasklist.ts
+++ routers/router-tasklist.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import * as utils from "./routerUtils";
 import {Tasklist} from "../ts-interfaces/model/Tasklist";
+import {StatusCodes} from "http-status-codes";
+import {ConnectionToDatabaseLostError} from "../ts-interfaces/errors/ConnectionToDatabaseLostError";
+import {DateFormatError} from "../ts-interfaces/errors/DateFormatError";
 
 export const tasklistRouter = express.Router();
 
@@ -22,11 +25,11 @@ tasklistRouter.post("/:userID", (req, res) => {
     const sortingOrder = req.body.sortingOrder ?? 1;
     const priority = req.body.priority ?? 1;
     if (utils.checkIfFalseSortingOrder(sortingOrder)){
-        res.status(400).send("sortingOrder must be a positive number");
+        res.status(StatusCodes.BAD_REQUEST).send("sortingOrder must be a positive number");
         return;
     }
     if (utils.checkIfFalsePriority(priority)){
-        res.status(400).send("priority must be a positive number");
+        res.status(StatusCodes.BAD_REQUEST).send("priority must be a positive number");
         return;
     }
     const result : Tasklist = {
@@ -38,5 +41,28 @@ tasklistRouter.post("/:userID", (req, res) => {
         isLocked: false,
         ownerID: userID
     };
-    res.status(201).json(result);
+    try {
+        //insertTask(database: sqlite3.Database, title: string, dueDate: string, description: string, priority: byte, tasklistID: number): void
+        //throws ConnectionToDatabaseLostError
+        //throws DateFormatError
+        //throws DateExpiredError
+        //throws IdNotFoundError (variablename)
+        //throws StringToLongError  (variablename)
+    } catch (err){
+        if (err instanceof ConnectionToDatabaseLostError){
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Database connection lost");
+            return;
+        }
+        if (err instanceof DateFormatError){
+            res.status(StatusCodes.BAD_REQUEST).send("Date format is incorrect");
+            return;
+        }
+        if (err instanceof DateExpiredError){
+            res.status(StatusCodes.BAD_REQUEST).send("Date is expired");
+            return;
+        }
+
+    }
+    res.status(StatusCodes.CREATED).json(result);
+    return;
 });
---
+++ ts-interfaces/errors/DateFormatError.ts
@@ -0,0 +1,3 @@
+export class DateFormatError extends Error {
+    //TODO: @Luca please Implement this class
+}
