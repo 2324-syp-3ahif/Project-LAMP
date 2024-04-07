978af3885af1ef11244471e20dfb87d7ff6f6331
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-04-07 16:55:24 +0200

    added utils to frontend

---
+++ public/src/utils.ts
@@ -0,0 +1,27 @@
+import * as crypto from 'crypto';
+
+export function checkMailFormat(mail: string): boolean{
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(mail);
+}
+
+export function checkPasswordFormat(password: string): boolean{
+    const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    return regex.test(password);
+}
+
+export function checkStringFormat(username: string): boolean{
+    const regex = /^[a-zA-Z0-9]{3,}$/;
+    return regex.test(username);
+}
+
+export function checkDateFormat(date: string): boolean {
+    const d = new Date(date);
+    return d.toString() != "Invalid Date";
+}
+
+export function hashString(str: string): string{
+    const hash = crypto.createHash('sha256');
+    hash.update(str);
+    return hash.digest('hex');
+}
