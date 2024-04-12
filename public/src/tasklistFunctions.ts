05ef2830ba8a99a5a710d5adb9ea465d2ea1659a
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-04-12 13:15:14 +0200

    big code refactor

--- public/src/tasklistFunctions.ts
+++ public/src/tasklistFunctions.ts
@@ -14,247 +14,247 @@ const orderCreateButton = document.getElementById('order-creation') as HTMLButto
 const filterButton = document.getElementById('filter-btn') as HTMLButtonElement;
 const filterTagsModal = document.getElementById('filter-tags-modal') as HTMLElement;
 const createForm = document.getElementById('create-form') as HTMLFormElement;
-const submitButton = document.getElementById('submit-btn') as HTMLButtonElement;
+const submitButton = document.getElementById('create-tasklist-btn') as HTMLButtonElement;
 const inviteUserBtn = document.getElementById('invite-user-btn') as HTMLButtonElement;
 const deleteTasklistBtn = document.getElementById('delete-tasklist-btn') as HTMLButtonElement;
 const deleteModal = document.getElementById('delete-modal') as HTMLElement;
 
-let deleteTasklistID = -1;
+let globalDeleteTasklistID = -1;
+let globalTasklists: Tasklist[] =[];
+let globalTags: Tag[] = [];
+let globalMail: string = "";
 
 export async function load(mail: string) {
     const listResp = await send(tasklistUrl + "email/" + mail, 'GET');
     const tagsResp = await send(tagUrl + mail, 'GET');
 
-    if (listResp.ok && tagsResp.ok) {
-        const lists: Tasklist[] = await listResp.json();
-        const tags: Tag[] = await tagsResp.json();
+    if (!listResp.ok || !tagsResp.ok) {
+        return;
+    }
 
-        await showAllTasklists(lists);
+    globalTasklists = await listResp.json();
+    globalTags = await tagsResp.json();
+    globalMail = mail;
 
-        deleteTasklistBtn.addEventListener('click', async () => {
-            if (deleteTasklistID !== -1) {
-                lists.splice(lists.findIndex((list: Tasklist) => list.tasklistID === deleteTasklistID), 1);
-                await send(tasklistUrl + deleteTasklistID, 'DELETE');
-                deleteTasklistID = -1;
-                deleteModal.style.display = "hide";
-                await showAllTasklists(lists);
-            }
+    await showAllTasklists();
+
+    createTasklistButton.addEventListener('click', async () => {
+        createForm.style.display = 'flex';
+    });
+
+    orderPriorityButton.addEventListener('click', async () => {
+        globalTasklists.sort((a: Tasklist, b: Tasklist) => {
+            return b.priority - a.priority;
+        });
+        await showAllTasklists();
+    });
+
+    orderViewButton.addEventListener('click', async () => {
+        /*lists.sort((a: Tasklist, b: Tasklist) => {
+            //return b.lastView.getTime() - a.lastView.getTime();
+        });*/
+        await showAllTasklists();
+    });
+
+    orderCreateButton.addEventListener('click', async () => {
+        globalTasklists.sort((a: Tasklist, b: Tasklist) => {
+            return a.tasklistID - b.tasklistID;
+        });
+        await showAllTasklists();
+    });
+
+    deleteTasklistBtn.addEventListener('click', deleteTask);
+    submitButton.addEventListener('click', createTasklist);
+    inviteUserBtn.addEventListener('click', invite);
+    filterButton.addEventListener('click', filterTasklists);
+}
+
+async function showAllTasklists() {
+    taskLists.innerHTML = "";
+    for (const list of globalTasklists) {
+        const listEl: HTMLElement = await showTasklist(list);
+        taskLists.appendChild(listEl);
+    }
+}
+
+async function showTasklist(list: Tasklist): Promise<HTMLElement> {
+    const listElement: HTMLElement = document.createElement('div');
+    listElement.classList.add('tasklist');
+    listElement.classList.add('card-body');
+    listElement.classList.add('card');
+
+    const title = document.createElement('h2');
+    title.innerHTML = list.title;
+    title.addEventListener('input', async () => {
+        list.title = title.innerHTML;
+        await send(tasklistUrl + list.tasklistID, 'PUT', list);
+        await showTasklist(list);
+    });
+    title.classList.add("card-title");
+
+    const tags = document.createElement('div');
+    tags.classList.add('tags');
+
+    const description = document.createElement('p');
+    description.innerHTML = list.description;
+    description.classList.add("card-text");
+
+    listElement.appendChild(title);
+    listElement.appendChild(description);
+    listElement.appendChild(tags);
+
+    listElement.addEventListener('click', () => {
+        if (globalDeleteTasklistID !== -1) {
+            return;
+        }
+        extendTasklist(listElement, list);
+    });
+
+    return listElement;
+}
+
+async function extendTasklist(listEl: HTMLElement, list: Tasklist) {
+    if (list.isLocked) {
+        list.isLocked = false; // just for testing purposes
+        await send(tasklistUrl + globalMail + "/" + list.tasklistID, 'PUT', list);
+        alert('This tasklist is locked');
+    } else {
+        list.isLocked = true;
+        await send(tasklistUrl + globalMail + "/" + list.tasklistID, 'PUT', list);
+
+        const tagsEl = document.createElement('div');
+        tagsEl.classList.add('tags');
+        /* tag creation must be implemented for this
+        const allTags: Tag[] = await (await send(tagUrl + list.tasklistID, 'GET')).json;
+        allTags.forEach((tag: Tag) => {
+            const tagElement = document.createElement('span');
+            tagElement.innerHTML = tag.name;
+            tagsEl.appendChild(tagElement);
+        }); */
+
+        const tasksEl = document.createElement('div');
+
+        const newTaskButton = document.createElement('button');
+        // TODO: add task element like in GUI mockups
+
+        const deleteButton = document.createElement('button');
+        deleteButton.id = "delete-button";
+        const binImg = document.createElement('img');
+        binImg.alt = 'filter img';
+        binImg.src = "./img/bin_icon.png";
+        binImg.id = 'filter-img';
+        deleteButton.appendChild(binImg);
+
+        deleteButton.classList.add('btn');
+        deleteButton.setAttribute('data-bs-toggle', 'modal');
+        deleteButton.setAttribute('data-bs-target', '#delete-modal');
+        deleteButton.addEventListener('click', () => {
+            globalDeleteTasklistID = list.tasklistID;
         });
 
-        createTasklistButton.addEventListener('click', async () => {
-            createForm.style.display = 'flex';
-        });
+        listEl.appendChild(tagsEl);
+        listEl.appendChild(tasksEl);
+        listEl.appendChild(deleteButton);
 
-        submitButton.addEventListener('click', async () => {
-            const title = (document.getElementById('name-input') as HTMLInputElement).value;
-            const description = (document.getElementById('description-input') as HTMLInputElement).value;
-            const priority = (document.getElementById('priority-input') as HTMLInputElement).value;
-            const sortingOrder = (document.getElementById('sorting-order-input') as HTMLInputElement).value;
-
-            if (title.length > 50) {
-                alert('Title is too long, must be less than 50 characters');
+        listEl.addEventListener('click', (e) => {
+            if (globalDeleteTasklistID !== -1) {
                 return;
-            } else if (description.length > 255) {
-                alert('Description is too long, must be less than 255 characters');
-                return;
             }
-
-            const tasklist: Tasklist = {
-                title: title,
-                description: description,
-                priority: parseInt(priority),
-                isLocked: false,
-                sortingOrder: parseInt(sortingOrder),
-                email: mail,
-                //lastView: new Date(),
-                tasklistID: await (await send(tasklistUrl + "nextID", "GET")).json(), // let server handle this
-            };
-            await send(tasklistUrl + mail, 'POST', tasklist);
-            lists.push(tasklist);
-            createForm.style.display = 'none';
-            await showAllTasklists(lists);
+            closeTasklist(list, listEl, tagsEl, tasksEl, deleteButton);
         });
 
-        inviteUserBtn.addEventListener('click', async () => {
-            const email = document.getElementById('email-input') as HTMLInputElement;
-            const emailText = email.value;
-            email.value = "";
-            if (checkMailFormat(emailText)) {
-                const nextId = await send(tasklistUrl + "nextID", 'GET');
-                await send("http://localhost:2000/api/mail/invite/" + emailText + "/" + nextId, 'POST', {email: email});
+        setTimeout(() => {
+            closeTasklist(list, listEl, tagsEl, tasksEl, deleteButton)
+        }, 120000); // close automatically after 2 minutes
+    }
+}
+async function closeTasklist(list: Tasklist, listEl: HTMLElement, tagsEl: HTMLElement, tasksEl: HTMLElement, deleteButton: HTMLElement) {
+    listEl.removeChild(tagsEl);
+    listEl.removeChild(tasksEl);
+    listEl.removeChild(deleteButton);
+    list.isLocked = false;
+    globalTasklists[globalTasklists.findIndex((tasklist: Tasklist) => tasklist.tasklistID === list.tasklistID)] = list;
+    await send(tasklistUrl + globalMail + "/" + list.tasklistID, 'PUT', list);
+}
+
+async function deleteTask() {
+    if (globalDeleteTasklistID !== -1) {
+        globalTasklists.splice(globalTasklists.findIndex((list: Tasklist) => list.tasklistID === globalDeleteTasklistID), 1);
+        await send(tasklistUrl + globalDeleteTasklistID, 'DELETE');
+        globalDeleteTasklistID = -1;
+        deleteModal.style.display = "hide";
+        await showAllTasklists();
+    }
+}
+
+async function invite() {
+    const email = document.getElementById('email-input') as HTMLInputElement;
+    const emailText = email.value;
+    email.value = "";
+    if (checkMailFormat(emailText)) {
+        const nextId = await send(tasklistUrl + "nextID", 'GET');
+        await send("http://localhost:2000/api/mail/invite/" + emailText + "/" + nextId, 'POST', {email: email});
+    } else {
+        alert('Invalid email address');
+    }
+}
+
+async function createTasklist() {
+    const title = (document.getElementById('name-input') as HTMLInputElement).value;
+    const description = (document.getElementById('description-input') as HTMLInputElement).value;
+    const priority = (document.getElementById('priority-input') as HTMLInputElement).value;
+    const sortingOrder = (document.getElementById('sorting-order-input') as HTMLInputElement).value;
+
+    if (title.length > 50) {
+        alert('Title is too long, must be less than 50 characters');
+        return;
+    } else if (description.length > 255) {
+        alert('Description is too long, must be less than 255 characters');
+        return;
+    }
+
+    const tasklist: Tasklist = {
+        title: title,
+        description: description,
+        priority: parseInt(priority),
+        isLocked: false,
+        sortingOrder: parseInt(sortingOrder),
+        email: globalMail,
+        //lastView: new Date(),
+        tasklistID: await (await send(tasklistUrl + "nextID", "GET")).json(), // let server handle this
+    };
+    await send(tasklistUrl + globalMail, 'POST', tasklist);
+    globalTasklists.push(tasklist);
+    createForm.style.display = 'none';
+    await showAllTasklists();
+}
+
+async function filterTasklists() {
+    console.log('filter');
+    const activeFilters: Tag[] = [];
+    for (const tag of globalTags) {
+        const tagElement = document.createElement('button');
+        tagElement.classList.add('tag-btn');
+        tagElement.classList.add('btn');
+        tagElement.innerHTML = tag.name;
+        tagElement.addEventListener('click', async () => {
+            if (tagElement.classList.contains('active')) {
+                tagElement.classList.remove('active');
+                activeFilters.splice(activeFilters.indexOf(tag), 1);
             } else {
-                alert('Invalid email address');
+                tagElement.classList.add('active');
+                activeFilters.push(tag);
             }
-        });
-
-        orderPriorityButton.addEventListener('click', async () => {
-            lists.sort((a: Tasklist, b: Tasklist) => {
-                return b.priority - a.priority;
-            });
-            await showAllTasklists(lists);
-        });
-
-        orderViewButton.addEventListener('click', async () => {
-            /*lists.sort((a: Tasklist, b: Tasklist) => {
-                //return b.lastView.getTime() - a.lastView.getTime();
-            });*/
-            await showAllTasklists(lists);
-        });
-
-        orderCreateButton.addEventListener('click', async () => {
-            lists.sort((a: Tasklist, b: Tasklist) => {
-                return a.tasklistID - b.tasklistID;
-            });
-            await showAllTasklists(lists);
-        });
-
-        filterButton.addEventListener('click', async () => {
-            console.log('filter');
-            const activeFilters: Tag[] = [];
-            for (const tag of tags) {
-                const tagElement = document.createElement('button');
-                tagElement.classList.add('tag-btn');
-                tagElement.classList.add('btn');
-                tagElement.innerHTML = tag.name;
-                tagElement.addEventListener('click', async () => {
-                    if (tagElement.classList.contains('active')) {
-                        tagElement.classList.remove('active');
-                        activeFilters.splice(activeFilters.indexOf(tag), 1);
-                    } else {
-                        tagElement.classList.add('active');
-                        activeFilters.push(tag);
-                    }
-                    for (const list of lists) {
-                        taskLists.innerHTML = "";
-                        const tagsOfList: Tag[] = (await send(tagUrl + list.tasklistID, 'GET')).json;
-                        if (activeFilters.length === 0) {
-                            await showAllTasklists(lists);
-                        } else if (activeFilters.every((tag: Tag) => tagsOfList.includes(tag))) {
-                            await showTasklist(list);
-                        }
-                    }
-                });
-                filterTagsModal.appendChild(tagElement);
-            }
-        });
-    }
-
-    async function showAllTasklists(lists: Tasklist[]) {
-        taskLists.innerHTML = "";
-        for (const list of lists) {
-            const listEl: HTMLElement = await showTasklist(list);
-            taskLists.appendChild(listEl);
-        }
-    }
-
-    async function extendTasklist(listEl: HTMLElement, list: Tasklist) {
-        if (list.isLocked) {
-            list.isLocked = false; // just for testing purposes
-            await send(tasklistUrl + mail + "/" + list.tasklistID, 'PUT', list);
-            alert('This tasklist is locked');
-        } else {
-            list.isLocked = true;
-            await send(tasklistUrl + mail + "/" + list.tasklistID, 'PUT', list);
-
-            const tagsEl = document.createElement('div');
-            tagsEl.classList.add('tags');
-            /* tag creation must be implemented for this
-            const allTags: Tag[] = await (await send(tagUrl + list.tasklistID, 'GET')).json;
-            allTags.forEach((tag: Tag) => {
-                const tagElement = document.createElement('span');
-                tagElement.innerHTML = tag.name;
-                tagsEl.appendChild(tagElement);
-            }); */
-
-            const tasksEl = document.createElement('div');
-            /* // TODO: fix
-            tasksEl.classList.add('tasks');
-            const tasksResp = await send(taskUrl + 'tasklistID/' +  list.tasklistID, 'GET');
-            if (tasksResp.ok)
-            const tasks: Task[] = await tasksResp.json();
-            *//*
-                for (const task of tasks) {
-                    const taskEl = document.createElement('div');
-                    // TODO show all tasks like in GUI mockups (with extended version)
-                    console.log("added task!");
+            for (const list of globalTasklists) {
+                taskLists.innerHTML = "";
+                const tagsOfList: Tag[] = (await send(tagUrl + list.tasklistID, 'GET')).json;
+                if (activeFilters.length === 0) {
+                    await showAllTasklists();
+                } else if (activeFilters.every((tag: Tag) => tagsOfList.includes(tag))) {
+                    await showTasklist(list);
                 }
-                 */
-            const newTaskButton = document.createElement('button');
-            // TODO: add task element like in GUI mockups
-
-            const deleteButton = document.createElement('button');
-            deleteButton.id = "delete-button";
-            const binImg = document.createElement('img');
-            binImg.alt = 'filter img';
-            binImg.src = "./img/bin_icon.png";
-            binImg.id = 'filter-img';
-            deleteButton.appendChild(binImg);
-
-            deleteButton.classList.add('btn');
-            deleteButton.setAttribute('data-bs-toggle', 'modal');
-            deleteButton.setAttribute('data-bs-target', '#delete-modal');
-            deleteButton.addEventListener('click', () => {
-                deleteTasklistID = list.tasklistID;
-            });
-
-            listEl.appendChild(tagsEl);
-            listEl.appendChild(tasksEl);
-            listEl.appendChild(deleteButton);
-
-            listEl.addEventListener('click', (e) => {
-                if (e.target !== listEl) {
-                    return;
-                }
-                closeTasklist(list, listEl, tagsEl, tasksEl, deleteButton);
-            });
-
-            setTimeout(() => {
-                closeTasklist(list, listEl, tagsEl, tasksEl, deleteButton)
-            }, 120000); // close automatically after 2 minutes
-        }
-    }
-
-    async function closeTasklist(list: Tasklist, listEl: HTMLElement, tagsEl: HTMLElement, tasksEl: HTMLElement, deleteButton: HTMLElement) {
-        listEl.removeChild(tagsEl);
-        listEl.removeChild(tasksEl);
-        listEl.removeChild(deleteButton);
-        list.isLocked = false;
-        await send(tasklistUrl + mail + "/" + list.tasklistID, 'PUT', list);
-    }
-
-    async function showTasklist(list: Tasklist): Promise<HTMLElement> {
-        const listElement: HTMLElement = document.createElement('div');
-        listElement.classList.add('tasklist');
-        listElement.classList.add('card-body');
-        listElement.classList.add('card');
-
-        const title = document.createElement('h2');
-        title.innerHTML = list.title;
-        title.addEventListener('input', async () => {
-            list.title = title.innerHTML;
-            await send(tasklistUrl + list.tasklistID, 'PUT', list);
-            await showTasklist(list);
-        });
-        title.classList.add("card-title");
-
-        const tags = document.createElement('div');
-        tags.classList.add('tags');
-
-        const description = document.createElement('p');
-        description.innerHTML = list.description;
-        description.classList.add("card-text");
-
-        listElement.appendChild(title);
-        listElement.appendChild(description);
-        listElement.appendChild(tags);
-
-        listElement.addEventListener('click', () => {
-            if (deleteTasklistID !== -1) {
-                return;
             }
-            extendTasklist(listElement, list);
         });
-
-        return listElement;
+        filterTagsModal.appendChild(tagElement);
     }
 }
