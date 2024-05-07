0fe9d2446eed1129cd29272e02557844150077dc
HuberAndreas1 <114642827+HuberAndreas1@users.noreply.github.com>
2024-05-07 10:47:09 +0200

    adusted send function
    adjusted for refresh token

--- public/src/sendUtils.ts
+++ public/src/sendUtils.ts
@@ -2,9 +2,9 @@ export function generateWarningPopUp(message: string, errorCode: number): void{
     alert("Error " + errorCode + ": " + message);
 }
 export async function send(route: string, method: "GET" | "POST" | "PUT" | "PATCH" | "DELETE", data?: object): Promise<any> {
-    let options: RequestInit = { method };
-    options.headers = { "Content-Type": "application/json" };
-    const jwt = sessionStorage.getItem('jwt');
+    let options: RequestInit = {method};
+    options.headers = {"Content-Type": "application/json"};
+    const jwt = localStorage.getItem('jwt');
     if (jwt !== null) {
         options.headers['Authorization'] = `Bearer ${jwt}`;
     }
@@ -13,12 +13,25 @@ export async function send(route: string, method: "GET" | "POST" | "PUT" | "PATC
     }
     const res = await fetch(route, options);
     if (res.status === 401) {
-        sessionStorage.removeItem('jwt');
-        generateWarningPopUp("Please authenticate!", res.status);
-        window.location.href = "/";
+        const refreshTokenRes = await fetch("http://localhost:2000/api/token/refresh", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            credentials: 'include' // Include cookies in the request
+        });
+        if (refreshTokenRes.ok) {
+            const newToken = await refreshTokenRes.json();
+            localStorage.setItem('jwt', newToken.accessToken);
+            return send(route, method, data);
+        } else {
+            console.error('Error:', await refreshTokenRes.text());
+            generateWarningPopUp(await refreshTokenRes.text(), refreshTokenRes.status);
+            // Redirect the user to the login page
+            window.location.href = "/";
+            return refreshTokenRes;
+        }
     } else if (!res.ok) {
         console.log(res.body);
-        console.error('Error:', res.text());
+        console.error('Error:', await res.text());
         generateWarningPopUp(await res.text(), res.status);
     }
     return res;
